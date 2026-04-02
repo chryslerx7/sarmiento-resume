0019570385b4a10cba6cf8a9d1f65fedb47f20a7
Jhed Sarmiento <chryslrosrmnt@gmail.com>
2026-04-02 11:23:15 +0800

    Fix import case for Portfolio component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Portfolio from "./portfolio";
+import Portfolio from "./Portfolio";
 
 function App() {
   return (
